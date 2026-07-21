be8f984c6f4d069196dc0069ed739658a4c000c9
starden44 <starden44@gmail.com>
2026-07-21 16:47:01 -0500

    Created television.py

---
+++ television.py
@@ -0,0 +1,54 @@
+class Television:
+    MIN_VOLUME = 0
+    MAX_VOLUME = 2
+    MIN_CHANNEL = 0
+    MAX_CHANNEL = 3
+
+    def __init__(self):
+        self.__status = False
+        self.__muted = False
+        self.__volume = self.MIN_VOLUME
+        self.__channel = self.MIN_CHANNEL
+        self.__prev_volume = self.__volume
+
+    def power(self):
+        self.__status = not self.__status
+
+    def mute(self):
+        if self.__status == True:
+            self.__muted = not self.__muted
+            self.__volume = self.MIN_VOLUME if self.__muted else self.__prev_volume
+
+    def channel_up(self):
+        if self.__status == True:
+            if self.__channel < self.MAX_CHANNEL:
+                self.__channel += 1
+            elif self.__channel == self.MAX_CHANNEL:
+                self.__channel = self.MIN_CHANNEL
+
+    def channel_down(self):
+        if self.__status == True:
+            if self.__channel > self.MIN_CHANNEL:
+                self.__channel -= 1
+            elif self.__channel == self.MIN_CHANNEL:
+                self.__channel = self.MAX_CHANNEL
+    def volume_up(self):
+        if self.__status == True:
+            if self.__muted == True:
+                self.__muted = False
+                self.__volume = self.__prev_volume
+            if self.__volume < self.MAX_VOLUME:
+                self.__volume += 1
+            self.__prev_volume = self.__volume
+
+    def volume_down(self):
+        if self.__status == True:
+            if self.__muted == True:
+                self.__muted = False
+                self.__volume = self.__prev_volume
+            if self.__volume > self.MIN_VOLUME:
+                self.__volume -= 1
+            self.__prev_volume = self.__volume
+
+    def __str__(self):
+        return f"Power = {self.__status}, Channel = {self.__channel}, Volume = {self.__volume}"
